a99e8c0a799eae8e57c066e9d033e19195479a0a
Andre Entrekin <andre.entrekin@gmail.com>
2022-01-04 16:32:55 -0600

    page design finished/some logic finished

---
+++ src/app/models/GiftCard.ts
@@ -0,0 +1,7 @@
+export interface GiftCard {
+    id: number;
+    cardNumber: number;
+    balance: number;
+    email: string;
+    
+}
---
+++ src/app/models/GiftObj.ts
@@ -0,0 +1,8 @@
+import { GiftCard } from "./GiftCard";
+
+export interface GiftObj {
+    giftCard: GiftCard;
+    fromName: string;
+    fromEmail: string;
+    message: string;
+}
