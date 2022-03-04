c770611428f352375319b3a7627764406d230d3d
Andre Entrekin <andre.entrekin@gmail.com>
2022-03-04 16:20:52 -0600

    added validators to forms

--- src/app/components/guided/guided.component.ts
+++ src/app/components/guided/guided.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Customer } from 'src/app/models/Customer';
 import { ApiService } from 'src/app/services/api.service';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-guided',
@@ -12,41 +13,28 @@ export class GuidedComponent implements OnInit {
 
   errorBoolean: boolean = false;
   isLoading: boolean = false;
+  submitted: boolean = false;  
 
-  firstName: string;
-  lastName: string;
-  email: string;
-  phone: string;
-  message: string;
-
-  constructor(private api: ApiService, private router: Router) { }
+  constructor(private api: ApiService, private router: Router) {
+    
+   }
 
   ngOnInit(): void {
-  }
 
-  submit(){
-    // validates empty fields
-    if(this.firstName == "" || this.lastName == "" || this.email == "" || this.phone == "" || this.message == "" || this.firstName == undefined || this.lastName == undefined || this.email == undefined || this.phone == undefined || this.message == undefined){
-      this.errorBoolean = true;
-    }else{
-      this.isLoading = true;
-      // constucts customer object
-      const customer:Customer = {
-        firstName: this.firstName,
-        lastName: this.lastName,
-        email: this.email,
-        phone: this.phone,
-        message: this.message
-      }
-      console.log(customer);
-      // sends customer object to backend
-      this.api.sendEmail("guided", customer).subscribe(res =>{
-        console.log(res)
-        this.isLoading = false;
-        this.router.navigate(['thank-you-email']);
-      });
-    }
   }
+  
+
+  custEventHandler(event: any){
+    const customer: Customer = event;
+    this.isLoading = true;
+    // sends customer object to backend
+    this.api.sendEmail("guided", customer).subscribe(res =>{
+      this.isLoading = false;
+      this.router.navigate(['thank-you-email']);
+    });
+  }
+  
+  
 
 
 }
--- src/app/components/lessons/lessons.component.ts
+++ src/app/components/lessons/lessons.component.ts
@@ -10,42 +10,21 @@ import { ApiService } from 'src/app/services/api.service';
 })
 export class LessonsComponent implements OnInit {
 
-  errorBoolean: boolean = false;
   isLoading: boolean = false;
-
-  firstName: string;
-  lastName: string;
-  email: string;
-  phone: string;
-  message: string;
 
   constructor(private api: ApiService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
-  submit(){
-    // validates empty fields
-    if(this.firstName == "" || this.lastName == "" || this.email == "" || this.phone == "" || this.message == "" || this.firstName == undefined || this.lastName == undefined || this.email == undefined || this.phone == undefined || this.message == undefined){
-      this.errorBoolean = true;
-    }else{
-      this.isLoading = true;
-      //constructs customer object
-      const customer:Customer = {
-        firstName: this.firstName,
-        lastName: this.lastName,
-        email: this.email,
-        phone: this.phone,
-        message: this.message
-      }
-      console.log(customer);
-      // sends customer object to backend
-      this.api.sendEmail("lessons", customer).subscribe(res =>{
-        console.log(res)
-        this.isLoading = false;
-        this.router.navigate(['thank-you-email']);
-      });
-    }
+  custEventHandler(event: any){
+    const customer: Customer = event;
+    this.isLoading = true;
+    // sends customer object to backend
+    this.api.sendEmail("lessons", customer).subscribe(res =>{
+      this.isLoading = false;
+      this.router.navigate(['thank-you-email']);
+    });
   }
 
 
